cfaf8b65319f741861f8b608d596ab25c6628507
Steve Fallet <steve.fallet@gmail.com>
2020-08-23 20:07:57 +0200

    Fichiers de base

---
+++ js/main.js
@@ -0,0 +1,20 @@
+/**
+ * Fichier de base pour application web HTML/CSS/JS
+ * @author  Steve Fallet
+ * @version 0.1
+ * @since   2019-08-19
+ */
+
+// http://www.jslint.com/help.html
+/*jslint this:true */
+/*jslint es6, browser, devel, for, single, multivar*/
+/*global window, document, alert, performance */
+
+(function main() {
+  "use strict"; // Demande un interprétation stricte du code
+
+  let a = "Bonjour";
+
+  console.log(a);
+}()); // Main IIFE
+
