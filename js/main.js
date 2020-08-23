7386d29f5939e492a04d3eb1c44432e9a2315a69
Steve Fallet <steve.fallet@gmail.com>
2020-08-23 20:46:37 +0200

    Validation JSHint

--- js/main.js
+++ js/main.js
@@ -5,15 +5,10 @@
  * @since   2019-08-19
  */
 
-// http://www.jslint.com/help.html
-/*jslint this:true */
-/*jslint es6, browser, devel, for, single, multivar*/
-/*global window, document, alert, performance */
-
 (function main() {
-  "use strict"; // Demande un interprétation stricte du code
+  'use strict'; // Demande un interprétation stricte du code
 
-  let a = "Bonjour";
+  let a = 'Bonjour';
 
   console.log(a);
 }()); // Main IIFE
